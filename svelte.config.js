9e1a99baca66bbb0dc21af62ad91df9e38049dfd
Your Name <u1karsh.copywriting@gmail.com>
2025-10-23 18:33:55 +0530

    4

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,22 +6,14 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
-
-		alias: {
-			// $lib: 'src/renderer/sveltekit/src/lib',
-			// $routes: 'src/renderer/sveltekit/src/routes',
-
-			// "@/*": "./path/to/lib/*",
-			// "@/components/*": "./src/shadcn-lib/components",
-			"@/*": "./src/shadcn-ui-lib/*",
-		},
-	},
+		alias: { // $lib: 'src/renderer/sveltekit/src/lib',
+		// $routes: 'src/renderer/sveltekit/src/routes',
+		// "@/*": "./path/to/lib/*",
+		// "@/components/*": "./src/shadcn-lib/components",
+		"@/*": "./src/shadcn-ui-lib/*" }
+	}
 };
 
 export default config;
